6de82cfff3cadc8439c6df04288fe0e69dae4e46
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-03 09:53:16 +0200

    init

---
+++ src/components/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Register() {
+    return (
+        <div>
+            <form>
+                <input type="email" />
+                <input type="password" />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
---
+++ src/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+
+import { useAuth, initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBIAw-vTA05L4qErMBiAhvbwI5RLzIm5k8",
+
+    authDomain: "task-aaa65.firebaseapp.com",
+
+    projectId: "task-aaa65",
+
+    storageBucket: "task-aaa65.appspot.com",
+
+    messagingSenderId: "1072364639649",
+
+    appId: "1:1072364639649:web:2bd3cbabc0a43f6004e7e0"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = useAuth();
