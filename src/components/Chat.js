ee47d5942fb88d98444b400b27281da316472902
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-04 15:44:48 +0200

    trying to implement sending msgs

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useLayoutEffect, useEffect, useState } from 'react'
 
 import { useAuth } from '../context/AuthContext'
 
 import { db } from '../firebase'
-import { getDocs, addDoc, collection } from 'firebase/firestore'
+import { addDoc, collection, doc, getDoc, getDocs, query } from 'firebase/firestore'
 
 export default function Chat() {
     const [text, setText] = useState('')
@@ -27,10 +27,14 @@ export default function Chat() {
         sendMessage(text);
     }
 
+    const mensajes = []
+
     return (
         <div>
             <h1>Chat</h1>
             <div className='chat-container'>
+
+
                 <form onSubmit={handleSubmit}>
                     <input onChange={(e) => { setText(e.target.value) }}></input>
                     <button>Send</button>
