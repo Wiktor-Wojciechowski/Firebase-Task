256524309e7ab01137b6bc41c9ed994f24250172
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-04 12:21:59 +0200

    added ability to send messages in chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { useAuth } from '../context/AuthContext'
 
+import { db } from '../firebase'
+import { getDocs, addDoc, collection } from 'firebase/firestore'
+
 export default function Chat() {
+    const [text, setText] = useState('')
+
+    async function sendMessage(msg) {
+        try {
+            var date = new Date();
+            const docRef = await addDoc(collection(db, "messages"), {
+                text: msg,
+                time: date
+            });
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        sendMessage(text);
+    }
 
     return (
         <div>
             <h1>Chat</h1>
             <div className='chat-container'>
-                <form>
-                    <input></input>
+                <form onSubmit={handleSubmit}>
+                    <input onChange={(e) => { setText(e.target.value) }}></input>
                     <button>Send</button>
                 </form>
             </div>
--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+import { getFirestore } from 'firebase/firestore'
+
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -30,6 +32,8 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
 
 export const auth = getAuth();
 export default app;
