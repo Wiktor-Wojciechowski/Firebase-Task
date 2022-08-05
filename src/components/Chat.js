202a8329d550bc04da9583c0f19c211ac4775204
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-05 15:11:31 +0200

    collection listener now works

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -11,22 +11,29 @@ export default function Chat() {
 
     const [message, setMessage] = useState('')
 
+    const [messages, setMessages] = useState([])
+
     const messagesRef = collection(db, 'messages')
+
+    var items = []
 
     //get data
     const getData = async () => {
         onSnapshot(messagesRef, (snapshot) => {
-            let items = []
+            items = []
             snapshot.docs.forEach((doc) => {
                 items.push({ ...doc.data(), id: doc.id })
             })
             console.log(items)
+            return items
         })
     }
 
     useEffect(() => {
-        getData();
-    })
+        getData().then(() => {
+            setMessages(items)
+        })
+    }, [])
 
     return (
         <div>{message}</div>
