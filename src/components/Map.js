70a5613e6ebbf56cd1007af190ea23788ba1329b
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-11 15:28:56 +0200

    added map and a marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,49 +1,90 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+import { db } from '../firebase'
+
 import L from 'leaflet'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
+
+//adds leaflet css with working images{
 import 'leaflet/dist/leaflet.css';
-
-
 
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
-    shadowUrl: iconShadow
+    shadowUrl: iconShadow,
+
+    //fixes marker inaccuracy
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
+// }
 
 export default function Map() {
 
+    const [users, setUsers] = useState([]);
+
     useEffect(() => {
-
+        const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
+            setUsers(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        })
         return () => {
-
+            unsubscribe();
         }
     }, [])
+
+
+
+
+
+    const [loading, setLoading] = useState(true);
+    const [coords, setCoords] = useState();
+
+    useEffect(() => {
+
+        navigator.geolocation.getCurrentPosition((pos) => {
+            console.log(pos.coords.latitude + ' ' + pos.coords.longitude)
+            setCoords([pos.coords.latitude, pos.coords.longitude])
+            setLoading(false)
+        });
+
+    }, [])
+
+
 
 
     return (
         <div className='map-component' >
             <div className='map-container'>
                 <div id='map'>
-                    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <MapContainer center={[54.54, 15.19]} zoom={4} worldCopyJump={true} scrollWheelZoom={true}>
                         <TileLayer
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
+
+                        {loading && <div style={{ margin: 'auto', position: 'absolute', top: 0, left: '50px', zIndex: 1000 }} >Loading...</div>}
+                        {coords && <Marker position={coords} />}
+
+
                         <Marker position={[51.505, -0.09]}>
                             <Popup>
                                 A pretty CSS3 popup. <br /> Easily customizable.
                             </Popup>
                         </Marker>
+                        <Marker position={[52.505, -0.09]}></Marker>
                     </MapContainer>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     )
 }
