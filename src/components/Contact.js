8867c03f02e3d1019f8a60c3ae1a28c39c71c1b0
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-18 09:44:13 +0200

    style changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
+import emailIcon from '../images/email-icon.svg'
+import phoneIcon from '../images/phone-outline-icon.svg'
+import markerIcon from '../images/map-pin-point-icon.svg'
+
 export default function Contact() {
     return (
         <div className='contact-component'>
             <h1>Contact us:</h1>
             <address>
-                <div><img alt='email icon'></img>Phone number: 123 456 789</div>
-                <div><img alt='phone icon'></img>Email: email@email.com</div>
-                <div><img alt='house icon'></img>Country, City, Street, Number</div>
+                <ul>
+                    <li><a href="tel:+123456789"><img src={phoneIcon} className='icon' alt='phone icon'></img><span>Phone number: 123 456 789</span></a></li>
+                    <li><a href="mailto:email@email.com"><img src={emailIcon} className='icon' alt='email icon'></img><span>Email: email@email.com</span></a></li>
+                    <li><img src={markerIcon} className='icon' alt='marker icon'></img><span>Country, City, Street, Number</span></li>
+                </ul>
             </address>
         </div>
     )
