778e252ab9dd4c024ba71491beab3ee5df57f055
WiktorAA <109503651+WiktorAA@users.noreply.github.com>
2022-08-30 15:52:35 +0200

    changes to star wars

--- src/components/StarWarsPeople.js
+++ src/components/StarWarsPeople.js
@@ -3,13 +3,22 @@ import { useState, useEffect } from 'react'
 
 export default function StarWarsPeople() {
 
+    fetch('https://swapi.dev/api/people/1').then(r => console.log(r))
+
+
+    useEffect(() => {
+        document.title = 'People of Star Wars'
+    }, [])
     const [page, setPage] = useState()
 
     useEffect(() => {
-        fetch('https://swapi.dev/api/people').then(data => data.json().then((p => {
-            setPage(p)
-            console.log(page)
-        })))
+        fetch('https://swapi.dev/api/people').then(data => {
+            console.log(data); data.json().then((p => {
+
+                setPage(p)
+                console.log(page)
+            }))
+        })
     }, [])
 
 
@@ -28,17 +37,16 @@ export default function StarWarsPeople() {
                 ))}
                 </div>
 
-                <div className='next-button' onClick={() => {
+                <div className='next-button'><span onClick={() => {
                     if (page.previous) {
                         fetch(page.previous).then(data => data.json().then((p => {
                             setPage(p)
                             console.log(page)
                         })))
                     }
-                }}>Previous</div>
+                }}>Previous</span></div>
 
-
-                <div className='previous-button' onClick={() => {
+                <div className='previous-button' ><span onClick={() => {
                     if (page.next) {
                         fetch(page.next).then(data => data.json().then((p => {
                             setPage(p)
@@ -46,7 +54,7 @@ export default function StarWarsPeople() {
                         })))
                     }
 
-                }}>Next</div>
+                }}>Next</span></div>
             </div>
 
         )
@@ -58,19 +66,35 @@ export default function StarWarsPeople() {
 }
 function Person(props) {
     const [show, setShow] = useState(false)
-    console.log(props.person.name)
-    return (
-        <div>
-            <div onClick={() => { setShow(!show) }}>{props.person.name}</div>
-            {show &&
-                Object.entries(props.person).map(property => (
-                    <div className={property[0]}>{property[1]}</div>
-                ))
 
+    return (
+        <div className='person'>
+            <h2 onClick={() => { setShow(!show) }}>{props.person.name}</h2>
+            {show && <div className='properties'>
+                <div className={'property '}>Name: {props.person.name}</div>
+                <div className={'property '}>Height: {props.person.height}</div>
+                <div className={'property '}>Weight: {props.person.mass}</div>
+                <div className={'property '}>Hair Color: {props.person.hair_color}</div>
+                <div className={'property '}>Skin Color: {props.person.skin_color}</div>
+                <div className={'property '}>Eye Color: {props.person.eye_color}</div>
+                <div className={'property '}>Birth Year: {props.person.birth_year}</div>
+                <div className={'property '}>Gender: {props.person.gender}</div>
+                <div className={'property '}>Homeworld: {props.person.homeworld}</div>
+                <div className={'property '}>Films: {props.person.films}</div>
+                <div className={'property '}>Species: {props.person.species}</div>
+                <div className={'property '}>Vehicles: {props.person.vehicles}</div>
+                <div className={'property '}>Starships: {props.person.starships}</div>
+                <div className={'property '}>Created: {props.person.created}</div>
+                <div className={'property '}>Edited: {props.person.edited}</div>
+                <div className={'property '}>URL: {props.person.url}</div>
+                {/*}
+                {Object.entries(props.person).map(property => (
+                    <div key={property} className={'property ' + property[0]}>{property[0]}: {property[1]}</div>
+                ))}
+                */}
+            </div>
             }
+
         </div>
     )
-}
-function PersonDetails(props) {
-
 }
